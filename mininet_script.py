cde29f8386ab2f2737b736e78e550c269093d386
Jon <jhonatancarv0@gmail.com>
2024-12-17 19:11:59 -0300

    Aumento da rede

--- mininet_script.py
+++ mininet_script.py
@@ -13,15 +13,36 @@ def setup_mininet():
     info("Criando switches...\n")
     s1 = net.addSwitch('s1')
     s2 = net.addSwitch('s2')
+    s3 = net.addSwitch('s3')
 
     info("Criando hosts...\n")
-    h1 = net.addHost('h1', ip='10.0.0.1')
-    h2 = net.addHost('h2', ip='10.0.0.2')
+    hosts = []
+    for i in range(1, 10):
+        host = net.addHost(f'h{i}', ip=f'10.0.0.{i}', mac=f'00:00:00:00:00:{i:02x}')
+        hosts.append(host)
+
+    
+    info("Desativando IPv6 nos hosts...\n")
+    for host in hosts:
+        host.cmd('sysctl -w net.ipv6.conf.all.disable_ipv6=1')
+        host.cmd('sysctl -w net.ipv6.conf.default.disable_ipv6=1')
+        host.cmd('sysctl -w net.ipv6.conf.lo.disable_ipv6=1')
+
 
     info("Criando links...\n")
-    net.addLink(h1, s1, bw=10, delay='5ms')
-    net.addLink(h2, s2, bw=10, delay='5ms')
+    # Conectando switches entre si
     net.addLink(s1, s2, bw=20, delay='2ms')
+    net.addLink(s1, s3, bw=20, delay='2ms')
+
+    # Conectando hosts aos switches
+    for i in range(3):  # h1, h2, h3 -> s1
+        net.addLink(hosts[i], s1, bw=10, delay='5ms')
+    for i in range(3, 6):  # h4, h5, h6 -> s2
+        net.addLink(hosts[i], s2, bw=10, delay='5ms')
+    for i in range(6, 9):  # h7, h8, h9 -> s3
+        net.addLink(hosts[i], s3, bw=10, delay='5ms')
+    
+
 
     info("Iniciando a rede...\n")
     net.start()
